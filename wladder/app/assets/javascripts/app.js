8b33314efbafd2ed9cbc1fca260b51452eeffe51
sterlingbaldwin <sbaldwin4@mail.csuchico.edu>
2015-10-08 11:34:00 -0700

    Mostly there

--- wladder/app/assets/javascripts/app.js
+++ wladder/app/assets/javascripts/app.js
@@ -1,87 +1,82 @@
-wladder = angular.module('wladder',[]).controller('MainCtrl', [
-'$scope', '$http', function($scope, $http){
-  $scope.show_button = function(button_num){
-  	if(button_num != 0){
-  		angular.element('#' + (button_num-1) + '_button').css({
-  			'opacity': '0'
-  		});
-  	}
-  	console.log("got a click on "+button_num);
-  	angular.element('#' + button_num + '_button').css({
-  		'opacity': '1'
-  	});
-  }
-  $scope.init = function(){
-	$http({
-		method: 'GET', 
-		url: "http://localhost:3000", 
-		headers: {
-		'Accept':'application/json'
+wladder = angular.module('wladder', []).controller('MainCtrl', [
+	'$scope', '$http',
+	function($scope, $http) {
+		$scope.show_button = function(button_num) {
+			if (button_num != 0) {
+				angular.element('#' + (button_num - 1) + '_button').css({
+					'opacity': '0'
+				});
+			}
+			console.log("got a click on " + button_num);
+			angular.element('#' + button_num + '_button').css({
+				'opacity': '1'
+			});
 		}
-	}).success(function(response){
-		$scope.start_word = response.start_word
-		$scope.end_word = response.end_word
-	});
-	$scope.victory = false;
-	$scope.checked = false;
-	$scope.attempts = ["", "", "", "", ""];
-  }
-  $scope.victory = false;
-  $scope.checked = false;
-  $scope.attempts = ["", "", "", "", ""];
-  $scope.refresh = function(){
-	$scope.init();
-  }
-  $scope.getNumber = function(num) {
-    return new Array(num);   
-  } 
-  $scope.distance = function(a, b){
-	if(a.length != b.length) return -1;
-	var distance = 0;
-	for(var i = 0; i < a.length; a++){
-		if(a[i] != b[i]){
-			distance++;
+		$scope.init = function() {
+			$http({
+				method: 'GET',
+				url: "http://baldwin.codes:3000",
+				headers: {
+					'Accept': 'application/json'
+				}
+			}).success(function(response) {
+				$scope.start_word = response.start_word
+				$scope.end_word = response.end_word
+			});
+			$scope.victory = false;
+			$scope.checked = false;
+			$scope.attempts = ["", "", "", "", ""];
+		}
+		$scope.victory = false;
+		$scope.checked = false;
+		$scope.attempts = ["", "", "", "", ""];
+		$scope.refresh = function() {
+			$scope.init();
+		}
+		$scope.getNumber = function(num) {
+			return new Array(num);
+		}
+		$scope.distance = function(a, b) {
+			if (a.length != b.length) return -1;
+			var distance = 0;
+			for (var i = 0; i < a.length; i++) {
+				if (a[i] != b[i]) {
+					distance++;
+				}
+			}
+			return distance;
+		}
+		$scope.check = function() {
+			$scope.victory = true;
+			$scope.attempts.forEach(function(element, index, array) {
+				if (index != 5 && element.length != 0 && array[index + 1].length == 0) {
+					if ($scope.distance(element, $scope.end_word) != 1) {
+						good = false;
+					}
+					return;
+				}
+				if (index == 0) {
+					var a = $scope.start_word.slice(0);
+					if ($scope.distance(a, element) != 1) {
+						good = false;
+					}
+					return;
+				}
+				if ($scope.distance(element, array[index - 1]) != 1) {
+					good = false;
+					return;
+				}
+				if (index == array.length - 1) {
+					if ($scope.distance(element, $scope.end_word) != 1) {
+						good = false;
+					}
+					return;
+				}
+			});
+			$scope.victory = good;
+			$scope.checked = true;
+			$("#checked-false").hide();
+			$("#checled-true").show();
 		}
 	}
-	return distance;  
-  }
-  $scope.is_good = function(i){
-  	var good = 0;
-	switch(i){
-		case 0:{
-			if($scope.distance($scope.start_word, $scope.attempts[i]) > 0) good = 1;
-			break;
-		}
-		default:{
-			if($scope.distance($scope.attempts[i-1], $scope.attempts[i]) > 0) good = 1;
-		}
-	}
-	return good;
-  }
- $scope.check = function(){
-	var good = true;
- 	$scope.attempts.forEach(function(element, index, array){
-		console.log(element, index, array);
-		if(index == 0){
-			if($scope.distance($scope.start_word, element) != 1){
-				good = false;
-			}
-			return;
-		} 
-		if($scope.distance(element, array[index - 1]) != 1){
-			good = false;
-			return;
-		}
-		if(index == array.length - 1){
-			if($scope.distance(element, $scope.end_word) != 1){
-				good = false;
-			}
-			return;
-		}
-	});
-	$scope.victory = good;
-        $scope.checked = true;
-	$("#checked-false").hide();
-	$("#checled-true").show();
- }
-}]);
+]);
