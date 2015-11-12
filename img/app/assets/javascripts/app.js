a85602911fa4101e8138d88b37930632e8e54fda
sterlingbaldwin <sbaldwin4@mail.csuchico.edu>
2015-11-11 16:44:06 -0800

    Fixes angular issues

--- img/app/assets/javascripts/app.js
+++ img/app/assets/javascripts/app.js
@@ -1,14 +1,16 @@
 var app = angular.module('app', []);
 
-app.controller('ImgCtrl', ['$scope','$http'], function($scope, $http){
-  $http({
-    method: 'GET',
-    url: "http://localhost:3000",
-    headers: {
-      'Accept': 'application/json'
-    }
-  }).success(function(response) {
+app.controller('ImgCtrl', ['$scope','$http', function($scope, $http){
+  console.log('asdf');
+  $scope.init = function(){
+    $http({
+      method: 'GET',
+      url: "http://localhost:3000",
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).success(function(response) {
 
-    console.log(response);
-  });
-});
+    });
+  }
+}]);
