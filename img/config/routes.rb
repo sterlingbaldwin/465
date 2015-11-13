1cc9173c45e64fd8e9f17bfd2e2309f81632c06d
sterlingbaldwin <sbaldwin4@mail.csuchico.edu>
2015-11-12 18:00:05 -0800

    adds app/views/devise

--- img/config/routes.rb
+++ img/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_for :users
   resources :images do
     resources :tags, :shallow => true
     resources :image_users, :shallow => true
